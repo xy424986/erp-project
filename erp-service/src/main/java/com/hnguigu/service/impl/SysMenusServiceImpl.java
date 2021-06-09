224f2dc30507cae8ffc5ff526ec0bcaf1ad4e920
hy <706377483@qq.com>
2021-06-09 09:31:53 +0800

    hhy commit

--- erp-service/src/main/java/com/hnguigu/service/impl/SysMenusServiceImpl.java
+++ erp-service/src/main/java/com/hnguigu/service/impl/SysMenusServiceImpl.java
@@ -15,19 +15,35 @@ public class SysMenusServiceImpl extends ServiceImpl<SysMenusMapper, SysMenus> i
     public List<SysMenus> queryAllMenus() {
         //查找父菜单id  ==0 的菜单集合
         QueryWrapper<SysMenus> queryWrapper = new QueryWrapper<SysMenus>();
-        queryWrapper.eq("PARENT_ID",0);
-        List<SysMenus> parents = this.list(queryWrapper);
+        queryWrapper.eq("PARENT_ID", 0);
+        List<SysMenus> parentMenus = this.list(queryWrapper);
 
         //循环菜单集合  把每一个父菜单id当成  pid 继续查询
-        for(SysMenus p:parents ){
+        for (SysMenus p : parentMenus) {
             queryWrapper = new QueryWrapper<SysMenus>();
-            queryWrapper.eq("PARENT_ID",p.getId());
-            List<SysMenus> childs = this.list(queryWrapper);
+            queryWrapper.eq("PARENT_ID", p.getId());
+            List<SysMenus> childMenus = this.list(queryWrapper);
 
-            p.setChildMenu(childs);
+            p.setChildMenu(childMenus);
+
+            for (SysMenus p1 : childMenus) {
+                queryWrapper = new QueryWrapper<SysMenus>();
+                queryWrapper.eq("PARENT_ID", p1.getId());
+                List<SysMenus> childMenus2 = this.list(queryWrapper);
+
+                p1.setChildMenu(childMenus2);
+
+                for (SysMenus p2 : childMenus2) {
+                    queryWrapper = new QueryWrapper<SysMenus>();
+                    queryWrapper.eq("PARENT_ID", p2.getId());
+                    List<SysMenus> childMenus3 = this.list(queryWrapper);
+
+                    p2.setChildMenu(childMenus3);
+                }
+            }
         }
 
-        return parents;
+        return parentMenus;
 
     }
 }
