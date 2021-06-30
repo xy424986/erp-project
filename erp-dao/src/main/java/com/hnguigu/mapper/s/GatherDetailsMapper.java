e2c7ad675cd90e9b99751c17d5dd6774c3659d95
Administrator <2394373769@qq.com>
2021-06-30 11:12:56 +0800

    skl commit

---
+++ erp-dao/src/main/java/com/hnguigu/mapper/s/GatherDetailsMapper.java
@@ -0,0 +1,9 @@
+package com.hnguigu.mapper.s;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hnguigu.vo.zsxvo.pojo.s.GatherDetails;
+
+public interface GatherDetailsMapper  extends BaseMapper<GatherDetails> {
+
+}
